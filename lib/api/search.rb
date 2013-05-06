b5467c9c87aa3f3899ca0f246fb3ab97f197c97f
Chad Feller <cfeller@gmail.com>
2013-05-05 18:45:21 -0700

    updated search.rb with additional search methods

--- lib/api/search.rb
+++ lib/api/search.rb
@@ -8,17 +8,47 @@ module ChurchCommunityBuilder
       																first_name: first_name
       																} 
                 }
-      reader = ChurchCommunityBuilder::IndividualListReader.new(options)
+      reader = IndividualListReader.new(options)
       IndividualList.new(reader.load_feed)
     end
 
+    # Leaving 'modified_since' as 'nil' will return all
+    # batches in the system.
+    #
+    # Specifying a date will return all batches created
+    # or modified since that date.
+    #
+    # Date format should be YYYY-MM-DD
     def self.search_for_batch_by_date(modified_since = nil)
       options = {:url_data_params => {srv: "batch_profiles", 
                                       modified_since: modified_since
                                       } 
                 }
-      reader = ChurchCommunityBuilder::BatchListReader.new(options)
+      reader = BatchListReader.new(options)
       BatchList.new(reader.load_feed)
+    end
+
+    # Returns a BatchList of all batches posted in specified date range.
+    # End date is optional.
+    #
+    # Date format should be YYYY-MM-DD
+    def self.search_for_batch_by_date_range(start_date, end_date = nil)
+      options = {:url_data_params => {srv: "batch_profiles_in_date_range", 
+                                      start_date: start_date,
+                                      end_date: end_date
+                                      } 
+                }
+      reader = BatchListReader.new(options)
+      BatchList.new(reader.load_feed)
+    end
+
+    def self.search_for_batch_by_id(batch_id)
+      # options = {:url_data_params => {srv: "batch_profile_from_id", 
+      #                                 id: batch_id
+      #                                 } 
+      #           }
+      reader = BatchReader.new(batch_id)
+      Batch.new(reader.load_feed)
     end
 
 	end
