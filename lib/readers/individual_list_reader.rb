48836aaa11e2a1d568104ef3f95c92c6a1661fb5
Chad Feller <cfeller@gmail.com>
2013-05-09 14:15:27 -0700

    added documentation

--- lib/readers/individual_list_reader.rb
+++ lib/readers/individual_list_reader.rb
@@ -1,20 +1,16 @@
 module ChurchCommunityBuilder
 
+
+  # This reader works with both Individual List and ValidIndividualList
+  #
   class IndividualListReader < ApiReader
 
     # Constructor.
     #
-    # @param page The page number to get. Default is 1.
-    # @param options A hash of options for requesting data from the server.
-    # @param [CacheAdapter] cacher (optional) The cacher to be used to cache data.
     def initialize(options = {}, cacher = nil)
-      # page = options[:page] || 1
       filter = options[:filter]
 
       @url_data_params = options[:url_data_params]
-      # @url_data_path = options[:url_data_path] || "/v1/People"
-
-      # @url_data_params.merge!({:page => page}) if page
       @url_data_params.merge!({:filter => filter}) if filter
 
     end
