d575c436d9536a9bc76e65f7ae2f45294228d103
Chad Feller <cfeller@gmail.com>
2013-05-09 20:56:57 -0700

    updated examples/individual.rb

--- examples/individual.rb
+++ examples/individual.rb
@@ -10,3 +10,15 @@ puts results_1.all_names
 results_2 = ChurchCommunityBuilder::Individual.load_by_id(663)
 puts results_2.first_name
 puts results_2.last_name
+
+# Grab all individuals
+all_valid_individuals = ChurchCommunityBuilder::Search.search_for_all_valid_individuals
+
+# all individuals is a ValidIndividualList.
+# we can use this to build an IndividualList
+mil = ChurchCommunityBuilder::MergeableIndividualList.new
+
+all_valid_individuals.each do |vi|
+	individual = ChurchCommunityBuilder::Individual.load_by_id(vi.id)
+	mil.add(individual)
+end
