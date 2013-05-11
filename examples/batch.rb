d56f4ec2288d989d0438b3e4e7c631ec3f926b0b
Chad Feller <cfeller@gmail.com>
2013-05-10 23:37:38 -0700

    updated examples/batch.rb

--- examples/batch.rb
+++ examples/batch.rb
@@ -1,15 +1,21 @@
-require 'json'
-require 'yaml'
-require 'typhoeus'
-require 'active_support/core_ext'
+#!/usr/bin/env ruby
 
-y = YAML.load(File.read("../config/ccb.yml"))
-resp1 = Typhoeus::Request.get(y[:url], 
-															params: {srv: "batch_profiles",
-																			 modified_since: "2013-05-01", 
-																			 }, 
-																       username: y[:username], 
-																       password: y[:password]
-																       )
-jresp1 = JSON.parse(Hash.from_xml(resp1.body).to_json)
-puts jresp1.inspect
+require File.dirname(__FILE__) + '/../lib/ccb_api.rb'
+
+# Load batch by id
+batch_1 = ChurchCommunityBuilder::Search.search_for_batch_by_id(469)
+
+# Pull TransactionList from given Batch
+trans_list_1 = ChurchCommunityBuilder::TransactionList.new(batch_1.transactions)
+
+# Search for transactions by date
+start_date = "2013-05-03"
+end_date = nil #optional
+batch_2 = ChurchCommunityBuilder::Search.search_for_batch_by_date_range(start_date,end_date)
+
+mtl = ChurchCommunityBuilder::MergeableTransactionList.new
+
+batch_2.each do |batch|
+	trans_list = ChurchCommunityBuilder::TransactionList.new(batch.transactions)
+	mtl.add(trans_list)
+end
