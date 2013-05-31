cd7eb4c0c33f96aa12f666e9105ce5d784858a2f
weshays <weshays@gbdev.com>
2013-05-31 15:45:09 -0700

    refactored transaction

--- lib/api/transaction.rb
+++ lib/api/transaction.rb
@@ -4,32 +4,32 @@ module ChurchCommunityBuilder
 
   class Transaction < ApiObject
 
-  	ccb_attr_accessor :id,
-											:campus,
-											:individual,
-											:date,
-											:grouping,
-											:payment_type,
-											:check_number,
-											:transaction_details,
-											:creator,
-											:modifier,
-											:created,
-											:modified
+    ccb_attr_accessor :id,
+                      :campus,
+                      :individual,
+                      :date,
+                      :grouping,
+                      :payment_type,
+                      :check_number,
+                      :transaction_details,
+                      :creator,
+                      :modifier,
+                      :created,
+                      :modified
 
-									
+                  
 
-  	def initialize(json_data = nil, options = {})
-			initialize_from_json_object(json_data) unless json_data.nil?
+    def initialize(json_data = nil, options = {})
+      initialize_from_json_object(json_data) unless json_data.nil?
 
-			if json_data["transaction"].nil?
-      	transaction_json = json_data
+      if json_data["transaction"].nil?
+        transaction_json = json_data
       else
-      	transaction_json = json_data["transaction"]
+        transaction_json = json_data["transaction"]
       end
       
-      	initialize_from_json_object(transaction_json) unless transaction_json.nil?
-  	end
+        initialize_from_json_object(transaction_json) unless transaction_json.nil?
+    end
 
     def campus_id
       self.campus["id"]
@@ -51,45 +51,45 @@ module ChurchCommunityBuilder
       self.transaction_details["transaction_detail"].is_a?(Array)
     end
 
-    def amount
-
-      if multiple_transactions?
-        trans_ary = []
-        self.transaction_details["transaction_detail"].each {|trans| trans_ary << trans["amount"]}
-        return trans_ary
-      
-      else
-        self.transaction_details["transaction_detail"]["amount"]
-      
-      end
-
-    end
-
-    def fund_name
-
-      if multiple_transactions?
-        trans_ary = []
-        self.transaction_details["transaction_detail"].each {|trans| trans_ary << trans["coa"]["content"]}
-        return trans_ary
-      
-      else
-        self.transaction_details["transaction_detail"]["coa"]["content"]
-
-      end
-
-    end
-
     def fund_id
       if multiple_transactions?
-        trans_ary = []
-        self.transaction_details["transaction_detail"].each {|trans| trans_ary << trans["coa"]["id"]}
-        return trans_ary
-      
+        self.transaction_details["transaction_detail"].each{ |trans| trans["coa"]["id"] }
       else
         self.transaction_details["transaction_detail"]["coa"]["id"]
-
       end
+    end    
 
+    def fund_name
+      if multiple_transactions?
+        self.transaction_details["transaction_detail"].collect{ |trans| trans["coa"]["content"] }
+      else
+        self.transaction_details["transaction_detail"]["coa"]["content"]
+      end
+    end
+
+    def amount
+      if multiple_transactions?
+        self.transaction_details["transaction_detail"].collect{ |trans| trans["amount"] }     
+      else
+        self.transaction_details["transaction_detail"]["amount"]      
+      end
+    end
+
+
+    def as_splits
+      vals = []
+      if multiple_transactions?
+        self.transaction_details["transaction_detail"].each_with_index do |trans, indx| 
+          vals << [{:fund_id => trans["coa"]["id"],
+                    :fund_name => trans["coa"]["id"],
+                    :amount => self.transaction_details["transaction_detail"][indx]["amount"]}]
+        end
+      else 
+        vals << [{:fund_id => self.transaction_details["transaction_detail"]["coa"]["id"],
+                  :fund_name => self.transaction_details["transaction_detail"]["coa"]["content"],
+                  :amount => self.transaction_details["transaction_detail"]["amount"]}]
+      end
+      return vals
     end
 
     # def self.load_by_id(transaction_id)
