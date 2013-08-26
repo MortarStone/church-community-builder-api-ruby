2059066b4e0c5242a3a4395c59724f68ee81f8d0
weshays <weshays@gbdev.com>
2013-08-26 15:50:54 -0700

    Added comment

--- lib/common.rb
+++ lib/common.rb
@@ -19,6 +19,10 @@ module ChurchCommunityBuilder
       Typhoeus::Request.delete(url, {:headers => headers, :params => params})
     end    
 
+# Need to account for this
+# {"ccb_api"=>{"request"=>{"parameters"=>{"argument"=>[{"name"=>"srv", "value"=>"batch_profiles_in_date_range"}, 
+#   {"name"=>"date_start", "value"=>"2013-03-11"}, {"name"=>"date_end", "value"=>"2013-04-10"}]}}, 
+#   "response"=>{"error"=>{"number"=>"005", "type"=>"Service Permission", "content"=>"Query limit of '10000' reached, please try again tomorrow."}}}}
 
     unless response.success?
       if response.code > 0
