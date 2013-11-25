fdbe665b6871c19fc8ba3656de3f716197e13cec
weshays <weshays@gbdev.com>
2013-11-24 18:09:29 -0800

    Updated to account for two different API formats in the batch list.

--- lib/api/batch_list.rb
+++ lib/api/batch_list.rb
@@ -14,16 +14,22 @@ module ChurchCommunityBuilder
                 :json_data #for debugging
 
     def initialize(json)
-      @json_data = json["ccb_api"]
-      @request_data = @json_data["request"]
-      @response_data = @json_data["response"]
-      
-      @service = @response_data["service_action"] #CCB service type accessed
-      @availability = @response_data["availability"]
-      @batches = @response_data['batches']
+      @json_data = json['ccb_api']
+      @request_data = @json_data['request']
 
-      @count = @batches["count"].to_i #number of records
-      @batch_array = @batches["batch"].class == Array ? @batches["batch"] : [@batches["batch"]].compact #array of each batch
+      # This is needed for now to account for the two different formats
+      @batches = if @json_data.has_key?('response')
+        @response_data = @json_data['response']      
+        @service = @response_data['service_action'] #CCB service type accessed
+        @availability = @response_data['availability']
+        # @batches = @response_data['batches']
+        @response_data['batches']
+      else
+        @json_data['batches']
+      end
+
+      @count = @batches['count'].to_i #number of records
+      @batch_array = @batches['batch'].class == Array ? @batches['batch'] : [@batches['batch']].compact #array of each batch
     end
 
     def all_names
